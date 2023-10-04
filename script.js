ddd32d39c7ed8b3b2b9c95c5da4056cd621f2d87
Brandon Garza <brandon@Brandons-MacBook-Air.local>
2023-10-04 14:57:31 -0500

    homepagehtml/css

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  
+  function goToHome() {
+    window.location.href = 'homepage.html';  // Replace 'home.html' with the actual URL of your home page
+  }
+  
+  function goToMenu() {
+    window.location.href = 'index.html';  // Replace 'menu.html' with the actual URL of your menu page
+  }
+  

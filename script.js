737f73246cba826f438f61eb16b3b927a145d0b3
Brandon Garza <brandon@Brandons-MacBook-Air.local>
2023-10-11 03:17:35 -0500

    slideshow

--- script.js
+++ script.js
@@ -8,3 +8,22 @@
     window.location.href = 'menu.html';  // Replace 'menu.html' with the actual URL of your menu page
   }
   
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+  
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+  
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }    
+  
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 4 seconds
+  }
